4208ab9fb3c7f00f44c8f54b6a8a6b7c0d8013d8
FrederikDhont <frederik.dhont@student.howest.be>
2025-08-27 18:45:31 +0200

    Bind DOM elements at app initialization

--- js/script.js
+++ js/script.js
@@ -2,7 +2,42 @@
 
 window.addEventListener("load", initialize);
 
+// Data variables
+const data = members;
 const sortOrder = ["Age", "Firstname", "Lastname"];
-const voices = ["Dan Castellaneta", "Nancy Cartwright", "Hank Azaria"];
+const voiceActors = ["Dan Castellaneta", "Nancy Cartwright", "Hank Azaria"];
 
-function initialize() {}
+// DOM elements
+let locationSelectEl;
+let sortSelectEl;
+let cardsWrapper;
+let detailsEl;
+let voiceActorButtons;
+let cardsPerVoiceActorWrapper;
+
+// App state
+const app = {
+  selectedLocationVal: "all",
+  selectedSortingVal: "no-sort",
+};
+
+/* ==========================
+   App initialization
+   ========================== */
+
+function initialize() {
+  // Initialize data
+  // Bind DOM elements
+  bindDOMElements();
+  // Initialize GUI
+  // Add event listeners
+}
+
+function bindDOMElements() {
+  locationSelectEl = document.getElementById("location-filter");
+  sortSelectEl = document.getElementById("sort");
+  cardsWrapper = document.getElementById("cards-main");
+  detailsEl = document.getElementById("details");
+  voiceActorButtons = document.getElementById("voice-actor-filter");
+  cardsPerVoiceActorWrapper = document.getElementById("cards-per-voice-actor");
+}
